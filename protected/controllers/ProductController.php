48da9a785a9ee8fce379c64a04204e0abf5310e2
nguyenphuochao <nguyenphuochao123@gmail.com>
2024-03-17 01:39:44 +0700

    m

--- protected/controllers/ProductController.php
+++ protected/controllers/ProductController.php
@@ -1,7 +1,6 @@
 <?php
 class ProductController extends Controller
 {
-
     // api list product
     public function actionIndex()
     {
@@ -12,6 +11,11 @@ class ProductController extends Controller
         $page = isset($_GET['page']) ? $_GET['page'] : 1;
         $limit = isset($_GET['limit']) ? $_GET['limit'] : 5;
         $offset = ($page - 1) * $limit;
+        $name = isset($_GET['name']) ? $_GET['name'] : '';
+        $search = "where 1";
+        if($name){
+            $search = "where products.name like '%$name%' ";
+        }
         $sql = "SELECT products.id,
                        products.name,
                        products.price,
@@ -19,7 +23,7 @@ class ProductController extends Controller
                        products.description,
                        categories.name as category_name
                      FROM products inner join categories
-                     on products.category_id = categories.id order by id desc LIMIT $offset,$limit";
+                     on products.category_id = categories.id $search  order by id desc LIMIT $offset,$limit";
         $products = Yii::app()->db->createCommand($sql)->queryAll(); // Viết code thuần lấy data
 
         $data = [
@@ -61,6 +65,62 @@ class ProductController extends Controller
             echo CJSON::encode($product->getErrors());
         }
     }
+    // api view product
+    public function actionViewProduct($id)
+    {
+        $request = Yii::app()->request; // lấy đối tượng request
+        if ($request->getRequestType() != 'GET') {
+            throw new CHttpException(400, 'Yêu cầu không hợp lệ');
+        }
+        $product = Product::model()->findByPk($id);
+        if ($product) {
+            $data = [
+                'status' => 200,
+                'mess' => 'Oke',
+                'data' => $product
+            ];
+            echo CJSON::encode($data);
+        } else {
+            echo CJSON::encode(['mess' => 'ID không tồn tại']);
+        }
+    }
+    // api update product
+    public function actionUpdateProduct($id)
+    {
+        $request = Yii::app()->request; // lấy đối tượng request
+        if ($request->getRequestType() != 'PUT') {
+            throw new CHttpException(400, 'Yêu cầu không hợp lệ');
+        }
+        $name = $request->getParam('name');
+        $price = $request->getParam('price');
+        $qty = $request->getParam('qty');
+        $description = $request->getParam('description');
+        $category_id = $request->getParam('category_id');
+        $product = Product::model()->findByPk($id);
+        if ($product) {
+            if ($name)
+                $product->name = $name;
+            if ($price)
+                $product->price = $price;
+            if ($qty)
+                $product->qty = $qty;
+            if ($description)
+                $product->description = $description;
+            if ($category_id)
+                $product->category_id = $category_id;
+
+            $product->save();
+            $data = [
+                'status' => 200,
+                'mess' => 'Oke',
+                'data' => $product
+            ];
+            echo CJSON::encode($data);
+        } else {
+            echo CJSON::encode(['mess' => 'ID không tồn tại']);
+        }
+    }
+    // api delete product
     public function actionDeleteProduct($id)
     {
         $request = Yii::app()->request; // lấy đối tượng request
